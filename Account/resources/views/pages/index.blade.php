7b8a7a75ced6bb8e8b931f39790c22dcd3fc5aee
pxmaciej <pxmaciej@outlook.com>
2020-12-05 23:10:54 +0100

    feat fonts

--- Account/resources/views/pages/index.blade.php
+++ Account/resources/views/pages/index.blade.php
@@ -1,4 +1,92 @@
 @extends('layouts.app')
 @section('content')
-index
+<section class="features-icons bg-light text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                    <div class="d-flex features-icons-icon"><i class="icon-book-open m-auto" data-bs-hover-animate="pulse" style="color: rgb(48,57,71);font-size:100px;"></i></div>
+                    <h3>&nbsp;RACHUNKOWOŚĆ<br></h3>
+                    <ul>
+                        <li style="text-align: left;">prowadzenie Podatkowej Księgi Przychodów i Rozchodów; Ewidencji Ryczałtowych<br></li>
+                        <li style="text-align: left;">prowadzenie<br>ewidencji środków trwałych oraz wartości niematerialnych i prawnych, ewidencji<br>wyposażenia<br></li>
+                        <li style="text-align: left;">&nbsp; &nbsp;prowadzenie ewidencji do celów rozliczeń z<br>tytułu podatku VAT<br></li>
+                        <li style="text-align: left;">rejestracja<br>działalności gospodarczej<br></li>
+                        <li style="text-align: left;">rozliczenia<br>z ZUS właścicieli firmy<br></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                    <div class="d-flex features-icons-icon"><i class="icon-calculator m-auto" data-bs-hover-animate="pulse" style="color: rgb(48,57,71);font-size:100px;"></i></div>
+                    <h3>PODATKI<br></h3>
+                    <ul>
+                        <li style="text-align: left;">obliczanie<br>wysokości należnych podatków dochodowych w ujęciu okresowym(zaliczki<br>miesięczne/kwartalne)<br></li>
+                        <li style="text-align: left;">sporządzanie<br>rocznych rozliczeń z tytułu uzyskiwanych dochodów osób fizycznych z<br>działalności gospodarczej<br></li>
+                        <li style="text-align: left;">sporządzanie<br>rocznych rozliczeń z tytułu działalności osób fizycznych w sferach<br>niezwiązanych z działalnością gospodarczą (dochody z tytułu pracy, umów<br>cywilnoprawnych, sprzedaży rzeczy i praw, dochody z kapitału finansowego,<br>dochody
+                            uzyskane za granicą)<br></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                    <div class="d-flex features-icons-icon"><i class="icon-briefcase m-auto" data-bs-hover-animate="pulse" style="color: rgb(48,57,71);font-size:100px;"></i></div>
+                    <h3 style="text-align: center;">KADRY<br>I PŁACE<br></h3>
+                </div>
+                <ul>
+                    <li style="text-align: left;">sporządzanie<br>i konsultowanie umów o pracę i umów cywilnoprawnych<br></li>
+                    <li style="text-align: left;">sporządzanie<br>miesięcznych list płac zawierających wszystkie pozycje wynagrodzeń<br></li>
+                    <li style="text-align: left;">prowadzenie<br>kartotek wynagrodzeń<br></li>
+                    <li style="text-align: left;">sporządzenie<br>rozliczeń rocznych dla pracowników/zleceniobiorców (PIT-37/PIT-36)<br></li>
+                </ul>
+            </div>
+            <div class="col-lg-3">
+                <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                    <div class="d-flex features-icons-icon"><i class="icon-cloud-upload m-auto" data-bs-hover-animate="pulse" style="color: rgb(48,57,71);font-size:100px;"></i></div>
+                    <h3>KSIĘGOWOŚĆ<br>ONLINE<br></h3>
+                </div>
+                <ul>
+                    <li style="text-align: left;">panel klienta<br></li>
+                    <li style="text-align: left;">możliwość dostarczania dokumentów w formie elektronicznej<br></li>
+                    <li style="text-align: left;">możliwość wystawiania faktur i innych dokumentów w systemie<br></li>
+                    <li style="text-align: left;">stały dostęp do danych finansowych firmy<br></li>
+                    <li style="text-align: left;">możliwość samodzielnego monitoringu płatności wobec US i ZUS<br></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="showcase">
+    <div class="container-fluid p-0">
+        <div class="row no-gutters">
+            <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background: url(&quot;assets/img/pexels-daria-shevtsova-2825936%20—%20kopia.jpg&quot;) bottom / cover no-repeat;"><span></span></div>
+            <div class="col-lg-6 my-auto order-lg-1 showcase-text">
+                <h2>Nasze Biuro Rachunkowe Oferuje:</h2>
+                <p class="lead mb-0">Bardzo poszukiwany przez klientów wgląd do wszystkich danych on-line. Zapewniamy stały i bezpieczny dostęp do danych księgowych oraz skanów dokumentów on-line. Dzięki księgowości on-line cały czas na bieżąco<br>możesz monitorować stan
+                    zobowiązań i należności, a także poziom kosztów i<br>przychodów.<br></p>
+            </div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-lg-6 text-white showcase-img" style="background: url(&quot;pexels-andrew-neel-2682452 (1) — kopia.jpg&quot;) center / cover no-repeat;"><span></span></div>
+            <div class="col-lg-6 my-auto order-lg-1 showcase-text">
+                <h2>Księgowość uproszczona on-line:<br></h2>
+                <p class="lead mb-0">każdemu klientowi udostępniamy bezpłatnie aplikację do wystawiania faktur sprzedażowych on-line. Aplikacja ta pozwala wystawić fakturę<br>sprzedaży, a także ją wydrukować lub wysłać bezpośrednio do odbiorcy. Na<br>dedykowanym panelu
+                    klienta można zobaczyć skany wszystkich zaksięgowanych<br>faktur kosztowych i zakupowych. Panel dla klienta umożliwia też kontrolowanie<br>zobowiązań i należności oraz prowadzenie kasy gotówkowej. Informację o<br>podatkach umieszczamy
+                    na panelu klienta, wysyłamy powiadomienie sms oraz<br>tradycyjnie e-mailem.<br></p>
+            </div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background: url(&quot;assets/img/pexels-moose-photos-1036641.jpg&quot;);background-size: cover;"><span></span></div>
+            <div class="col-lg-6 my-auto order-lg-1 showcase-text">
+                <h2>Księgowość uproszczona tradycyjna:&nbsp;<br></h2>
+                <p class="lead mb-0">odbieramy w biurze lub możliwy jest<br>przyjazd po faktury do firmy w Legnicy i okolicach (dodatkowa płatność).<br>Wszystkie wiadomości dotyczące zobowiązań podatkowych przekazujemy e-mailem lub<br>SMS.<br></p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="testimonials text-center bg-light">
+    <div class="container">
+        <h2 class="mb-5">O nas</h2>
+    </div>
+</section>
 @endsection
