6878afe49b24048609f552a0ab14c2e5335883df
pxmaciej <pxmaciej@outlook.com>
2020-12-05 22:48:51 +0100

    first deploy

---
+++ Account/resources/views/includes/header.blade.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-light navbar-expand navigation-clean" style="background: rgb(48,57,71)!important;">
+    <div class="container"><img style="padding: 0px;height: 60px;" src={{ asset('image/LogoBiuroMale.png') }}><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"></button>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav ml-auto" style="font-size: 18px;">
+                <li class="nav-item"><a class="nav-link active" href="#" style="color: rgb(255,255,255);"><strong>Biuro</strong></a></li>
+                <li class="nav-item"><a class="nav-link active" href="#" style="color: rgb(255,255,255);"><strong>O nas</strong></a></li>
+                <li class="nav-item"><a class="nav-link active" href="#" style="color: rgb(255,255,255);"><strong>Oferta</strong></a></li>
+                <li class="nav-item"><a class="nav-link active" href="#" style="color: rgb(255,255,255);"><strong>Kontakt</strong></a></li>
+            </ul>
+        </div>
+        <ul class="list-inline mb-0">
+            <li class="list-inline-item"><a href="#"><i class="fa fa-facebook fa-2x fa-fw" style="color: rgb(255,255,255);"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fa fa-twitter fa-2x fa-fw" style="color: rgb(255,255,255);"></i></a></li>
+            <li class="list-inline-item"><a href="#"></a></li>
+        </ul><a class="btn btn-primary ml-auto" role="button" href="#">Zaloguj</a></div>
+</nav>
+<header class="masthead text-white text-center" style="background: url({{ URL::asset('/image/baner.jpg') }});background-size: cover;">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-9 mx-auto" style="margin: 0px;">
+                <h1 class="mb-5" style="text-align: center;">Usługę<br>księgowości uproszczonej kierujemy do małych firm prowadzących podatkową księgę<br>przychodów i rozchodów lub ryczałt od przychodów ewidencjonowanych<br></h1>
+            </div>
+            <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+                <form>
+                    <div class="form-row">
+                        <div class="col-12 col-md-9 mb-2 mb-md-0"><input class="form-control form-control-lg" type="email" placeholder="Wpisz email"></div>
+                        <div class="col-12 col-md-3"><button class="btn btn-primary btn-block btn-lg" type="Sign-up">Zapisz się!</button></div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</header>
