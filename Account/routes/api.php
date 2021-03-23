65a37dc904cc2fe2169ab6b639b645879c8ba3ec
pxmaciej <56204020+pxmaciej@users.noreply.github.com>
2021-03-23 19:22:50 +0100

    edit jwt

--- Account/routes/api.php
+++ Account/routes/api.php
@@ -21,7 +21,7 @@ Route::group([
 ], function ($router) {
 
     Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
-    Route::post('register', [App\Http\Controllers\AuthController::class,'register']);
+    Route::post('register', [App\Http\Controllers\AuthController::class, 'register']);
     Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout']);
     Route::post('refresh', [App\Http\Controllers\AuthController::class, 'refresh']);
     Route::post('profile', [App\Http\Controllers\AuthController::class, 'profile']);
